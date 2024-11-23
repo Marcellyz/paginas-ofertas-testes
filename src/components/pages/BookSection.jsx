4b2279cf8614ab56eebcf6833bd0030b4c569417
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-11-23 14:07:29 -0300

    rafactor: atualizando BookSection

--- src/components/pages/BookSection.jsx
+++ src/components/pages/BookSection.jsx
@@ -1,13 +1,11 @@
-import React from "react";
 import "../css/BookSection.css"; 
-import ProductReview from "../../../../paginas-ofertas-testes-main/src/components/ProductReview.jsx"; 
+
 import BookDetails from "../BookDetails.jsx";
 import CustomerReviews from "../CustomerReviews.jsx";
 
 const BookSection = () => {
   return (
     <div className="book-section">
-      <ProductReview />
       <BookDetails />
       <CustomerReviews /> 
     </div>
